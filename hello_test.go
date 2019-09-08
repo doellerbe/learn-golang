231547a6edb2356bc9e95786b1173fe9a8512f5e
dorian <doellerbe06@gmail.com>
2019-09-07 20:20:01 -0400

    Passing test example

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello, World!"
+	want := "Hello, world."
 	if got := Hello(); got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
