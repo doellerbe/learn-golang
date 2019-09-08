5004ec5841d529957b653f152c2cce17e98756eb
dorian <doellerbe06@gmail.com>
2019-09-07 22:00:13 -0400

    Type conversion

--- hello_types/types.go
+++ hello_types/types.go
@@ -8,4 +8,14 @@ type hotdog int
 func main() {
 	b := hotdog(12)
 	fmt.Println(b)
+
+	// Convert a type
+	a := int(b)
+	fmt.Println(a)
+
+	// assign a new value to a
+	a = 9999
+	fmt.Println(a)
+
+	// a = hotdog(20) // cannot assign to type hotdog anymore!
 }
